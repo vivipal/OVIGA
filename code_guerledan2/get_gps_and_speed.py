a6a3a9b9ae33d47efd82b057b291b406b4a97ce4
Virgile P <adtoviv@gmail.com>
2022-02-22 11:12:04 +0100

    new try

--- code_guerledan2/get_gps_and_speed.py
+++ code_guerledan2/get_gps_and_speed.py
@@ -12,21 +12,12 @@ gps = gpsdrv.GpsIO()
 while 1 :
 
 
-    gps_data = gps.read_gll()
+    v = read_next_message(self)
 
-    # f = open("gps","w")
-    # f.write("{} {}".format(lat,lon))
-    # f.close()
+    print(v)
 
-    gll_ok,gll_data=gps.read_gll_non_blocking()
-    if gll_ok:
-        lat,lon = cvt_gll_ddmm_2_dd(gps_data)
-        print("coord = " + str(lat) + str(lon)))
-    time.sleep(0.01)
-
-    vtg_ok,vtg_data=gps.read_vtg_non_blocking()
-    if vtg_ok:
-        print("speed = " + str(vtg_data)))
     time.sleep(0.1)
+
+
 
 gps.close()
