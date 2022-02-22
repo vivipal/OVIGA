24c1b5858d0741029e3b6d224dc584efe21ad3f4
bettonga <gabriel.betton@gmail.com>
2022-02-22 17:59:39 +0100

    added cart2coord

--- code_guerledan2/ddboat_tools.py
+++ code_guerledan2/ddboat_tools.py
@@ -181,6 +181,24 @@ def coord2cart(coords,coords_ref=(48.198802, -3.014848)):
 
     return np.array([[x_tilde,y_tilde]]).T
 
+def cart2coord(pos,coords_ref=(48.198802, -3.014848)):
+    '''
+    in :
+        coords = (lat,lon)
+        coords_ref = centre du plan
+    return :
+        x_tilde et y_tilde coord sur le plan centre en coord_ref
+    '''
+
+
+    x,y = pos
+    lym,lxm = coords_ref
+
+    ly = y/R + lym
+    lx = lxm + x/(R*cos(ly))
+
+    return np.array([[ly,lx]]).T
+
 def follow_line(a,b,m,r=6) :
     '''
     in :
