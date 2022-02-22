752338ccf7712bb14e0f961601f874147e182b8a
AugustinMorge <augustin.morge@ensta-bretagne.org>
2022-02-22 14:50:47 +0100

    ajout lissajou

--- code_guerledan2/lissajou.py
+++ code_guerledan2/lissajou.py
@@ -25,7 +25,7 @@ def control_fb(x,w,dw,ddw):
 
 def control(x,w,dw):
     x1, x2, x3, x4 = x.flatten()
-
+    x4 = 0.5
     Ax = array([[cos(x3), -x4*sin(x3)],
                 [sin(x3), x4*cos(x3)]])
     y = array([[x1],
@@ -39,6 +39,24 @@ def control(x,w,dw):
     u = inv(Ax)@ddy
 
     return u
+
+def lissajou(x):
+    a1, a2 = 48.19881166666667,-3.0156366666666665
+    ax=init_figure(a1-50,a1+50,a2-50,a2+50)
+    dt = 0.02
+    R2 = 30
+    T = 2*pi
+    N = 1
+    nbato = 1
+
+    # for t in arange(0,30,dt) :
+
+    k = 2*pi/T
+    phi = 2*pi*nbato/N
+    w=array([[a1 + R2*cos(k*t + phi)], [a2 + R2*sin(k*t + phi)]])
+    dw=array([[-R2*k*sin(k*t + phi)], [k*R2*cos(k*t + phi)]])
+    ddw=array([[-R2*k**2*cos(k*t + phi)], [-k**2*R2*sin(k*t + phi)]])
+    return(control(x,w,dw))
 
 if __name__ == "__main__":
     a1, a2 = 48.19881166666667,-3.0156366666666665
@@ -62,6 +80,8 @@ if __name__ == "__main__":
         ddw=array([[-R2*k**2*cos(k*t + phi)], [-k**2*R2*sin(k*t + phi)]])
         draw_disk(ax,w,0.5,"red")
         u=control(x,w,dw)
+        print(u)
+        pause(1)
         # u=control_fb(x,w,dw,ddw)
         x = x + dt*f(x,u)
         draw_tank(x,'red')
